7d7bcb282cbf8a373011520b1df4981ce6cc6049
misbah0332 <misbah0haque@gmail.com>
2024-10-27 14:12:11 +0500

    Delete employee_api/employee_project directory

--- employee_api/employee_project/employee_project/router.py
+++
@@ -1,8 +0,0 @@
-from employee_register.viewsets import EmployeeViewset
-from rest_framework import routers
-
-router = routers.DefaultRouter()
-router.register('employee', EmployeeViewset)
-
-#localhost:path/api/
-#GET, POST, PUT, DELETE
